5488085ca54c637f21130904d7f5109fa5e4566f
franck-tchassi <franck.tchassi@laplateforme.io>
2025-07-21 13:44:10 +0200

    initialization local add

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,20 @@
+import Header from '@/components/Header';
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode;
+}
+
+const LandingLayout = ({children}: Props) => {
+  return (
+    <div>
+      <Header />
+      <main className="min-h-screen bg-background">
+        {children}
+      </main>
+
+    </div>
+  )
+}
+
+export default LandingLayout
